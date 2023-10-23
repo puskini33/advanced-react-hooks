1db7b538528191a2dc82d09b11612ff649db317a
Elena Hirjoaba <elena.hirjoaba@stackfuel.com>
2023-10-23 20:07:20 +0200

    refactor to use custom hook that throws error if child not wrapped in provider

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -1,37 +1,46 @@
 // useContext: simple Counter
 // http://localhost:3000/isolated/exercise/03.js
 
-import * as React from 'react'
+import * as React from 'react';
 import {useState} from "react";
 
-const CountContext = React.createContext()
+const CountContext = React.createContext();
 
 function CountProvider(props) {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-    return <CountContext.Provider value={{count, setCount}} {...props}/>
+    return <CountContext.Provider value={{count, setCount}} {...props}/>;
+}
+
+function useCount(){
+    const context = React.useContext(CountContext)
+
+    if (!context) throw Error("Child must be wrapped in the CountContext Provider to have access to the context.")
+
+    return context
+
 }
 
 function CountDisplay() {
 
-  const {count} = React.useContext(CountContext)
-  return <div>{`The current count is ${count}`}</div>
+    const {count} = useCount()
+    return <div>{`The current count is ${count}`}</div>;
 }
 
 function Counter() {
-    const {setCount} = React.useContext(CountContext)
-    const increment = () => setCount(c => c + 1)
-    return <button onClick={increment}>Increment count</button>
+    const {setCount} = useCount()
+    const increment = () => setCount(c => c + 1);
+    return <button onClick={increment}>Increment count</button>;
 }
 
 function App() {
 
     return (
-    <CountProvider >
-      <CountDisplay />
-      <Counter />
+    <CountProvider>
+        <CountDisplay/>
+        <Counter/>
     </CountProvider>
-  )
+    );
 }
 
-export default App
+export default App;
